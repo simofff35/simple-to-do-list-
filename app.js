48b59f79a4f758ea63d84fcc2423439afa9c5c11
simofff35 <simofff0@gmail.com>
2023-04-01 21:28:20 +0100

    i comleted a simple to do list v1 but it need a lot of work

--- app.js
+++ app.js
@@ -2,38 +2,48 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const {render} = require('express/lib/response');
+const date = require(`${__dirname}/date.js`);
+
+console.log(date.getDay());
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 
-let tasks = ['Buy food', 'Cook food', 'Eat food'];
+let tasks = [];
+let worktasks = [];
 
 app.get('/', (req, res) => {
-  let today = new Date();
-  let options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-  };
-  console.log(today.toLocaleDateString('en-US', options));
-
-  let day = today.toLocaleDateString('en-US', options);
-
   res.render('list', {
-    dayNow: day,
+    titleejs: date.getDate(),
     tasksejs: tasks,
+    path: '/',
   });
 });
 
 app.post('/', (req, res) => {
   task = req.body.taskInput;
-  tasks.push(task);
-  res.render('list', {tasksejs: tasks});
+
+  if (req.body.list == 'work') {
+    worktasks.push(task);
+    res.redirect('/work');
+  } else {
+    tasks.push(task);
+    res.redirect('/');
+  }
+});
+
+app.get('/work', (req, res) => {
+  res.render('list', {titleejs: 'Work List', tasksejs: worktasks, path: '/work'});
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
 });
 
 const port = 3000;
 app.listen(port, function () {
-  console.log('listening on port ' + port);
+  console.log(`listening on port ${port}`);
 });
